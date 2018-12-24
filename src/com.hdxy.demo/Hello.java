c1c1cd5aebf626bbb4f6009546f004863f6b054e
chanhuiwumeng <523489273@qq.com>
2018-12-24 11:43:05 +0800

    Initial commit

---
+++ src/com.hdxy.demo/Hello.java
@@ -0,0 +1,14 @@
+package com.hdxy.demo;
+
+/**
+ * @ClassName: Hello
+ * @Description: 测试
+ * @Author: 一尘
+ * @Date: 2018 年 12 月 24 11:41
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
